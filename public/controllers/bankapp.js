62f991caee07949dc4dc3bab791228f44dfeaa85
Marcode777 <marcosebello7@gmail.com>
2016-12-20 16:27:21 -0500

    bankapp is now fully functional

---
+++ public/controllers/bankapp.js
@@ -0,0 +1,9 @@
+TweenMax.from("h1", 1.5, {x:-600, ease:Back.easeOut});
+
+// function validateForm(){
+//   var x = document.forms["myForm"]["fname"].value;
+//   if(x == ""){
+//     alert("Fill this out!");
+//     return false;
+//   }
+// }
