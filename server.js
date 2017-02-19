5ad898873608e995720ee8863b1f5a9782313cd3
Marcode777 <marcosebello7@gmail.com>
2017-02-19 00:07:32 -0500

    snowboarding commit 2

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.post('/register', function (req, res) { // this is for user registration
        return res.status(200).json({
         status: 'Registration successful!'
       });
-       // return res.redirect(200, 'https://www.google.com'); // this needs to actually redirect
+       return res.redirect(200, 'https://www.google.com'); // this needs to actually redirect
        // can also .redirect(200, 'https://www.google.com'); right after res.
        
 
